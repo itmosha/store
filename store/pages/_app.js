f480e660e8b02e097980088e8ed96a43d2af919d
itmosha <itmosha0@gmail.com>
2022-12-16 19:54:56 +0300

    changed: perfect default hero-banner

--- store/pages/_app.js
+++ store/pages/_app.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Layout } from '../components'
 import '../styles/globals.css';
+import '../styles/product.css';
+import '../styles/header.css';
+import '../styles/footer.css';
+import '../styles/side-cart.css';
+import '../styles/hero-banner.css';
+import '../styles/footer-banner.css';
 import { StateContext } from '../context/StateContext';
 import { Toaster } from "react-hot-toast";
 
