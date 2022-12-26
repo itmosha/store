ee5c0598a7f763d6bded7c434c5e51e1fdeb9e5f
victor <vi350@inbox.ru>
2022-12-26 20:01:01 +0300

    fix: footer bug

--- store_backend/main/views.py
+++ store_backend/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import *
 
 from .models import Item
@@ -19,6 +22,16 @@ class ItemsViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ItemsSerializer
     lookup_field = 'slug'
     permission_classes = [permissions.BasePermission]
+
+
+class BasicAPI(APIView):
+    def post(self, request):
+        req_data = request.data
+        data = {
+            'first_name': req_data.get('first_name'),
+            'last_name': req_data.get('last_name')
+        }
+        return Response(data, status=status.HTTP_200_OK)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
