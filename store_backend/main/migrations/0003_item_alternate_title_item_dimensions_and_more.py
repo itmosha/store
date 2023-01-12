3e7d3e40f22552b8e5dd30968ddf4a7bfd156255
victor <vi350@inbox.ru>
2023-01-12 17:44:29 +0300

    feat: sku slug automatically filled up

--- store_backend/main/migrations/0003_item_alternate_title_item_dimensions_and_more.py
+++ store_backend/main/migrations/0003_item_alternate_title_item_dimensions_and_more.py
@@ -3,6 +3,13 @@
 from django.db import migrations, models
 
 
+def update_item_sku(apps, schema_editor):
+    item = apps.get_model('main', 'Item')
+    for item in item.objects.all():
+        item.sku = item.slug
+        item.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -40,6 +47,9 @@ class Migration(migrations.Migration):
             name='sku',
             field=models.CharField(blank=True, max_length=100),
         ),
+
+        migrations.RunPython(update_item_sku, reverse_code=migrations.RunPython.noop),
+
         migrations.AddField(
             model_name='item',
             name='weight',
--- store_backend/main/models.py
+++ store_backend/main/models.py
@@ -1,6 +1,7 @@
 import random
 import uuid
 from django.db import models
+from django.db.models.signals import pre_save
 from phonenumber_field.modelfields import PhoneNumberField
 
 
