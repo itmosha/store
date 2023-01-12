e6ef9a612c3831e8dfb9641764cc674641e46e0a
victor <vi350@inbox.ru>
2023-01-12 19:03:42 +0300

    fix!: populating with uuid added

--- store_backend/main/urls.py
+++ store_backend/main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from django.conf import settings
 
 router = DefaultRouter()
 router.register(r'items', views.ItemsViewSet)
