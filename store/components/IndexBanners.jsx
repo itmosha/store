03e7652e499628ef0926ecd2837aff754477b5b0
itmosha <itmosha0@gmail.com>
2023-02-04 12:59:41 +0300

    chaged: index banners into a single component

---
+++ store/components/IndexBanners.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Box, Flex, Image } from "@chakra-ui/react";
+import Link from "next/link";
+import SetsReferenceImage from "../public/SetsReference.png";
+import PartsReferenceImage from "../public/PartsReference.png";
+import MinifiguresReferenceImage from "../public/MinifiguresReference.png";
+import SeriesReferenceImage from "../public/SeriesReference.png";
+
+const IndexBanners = () => {
+    return (
+        <Box py={['10vw', '5vw', '0.5vw']}>
+            <Flex mx={['3vw', '10.5vw']} w={['94vw', '62vw']} maxW={'100vw'} gap={['2vw', '1vw']}>
+                <Box
+                    boxShadow={['0px 1px 5px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'} w={['30vw', '20vw']}>
+                    <Link href={'/sets/'}>
+                        <Image
+                            src={ SetsReferenceImage.src }
+                            borderRadius={'1rem'}
+                        />
+                    </Link>
+                </Box>
+                <Box
+                    boxShadow={['0px 1px 5px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'} w={['30vw', '20vw']}>
+                    <Link href={'/parts'}>
+                        <Image
+                            src={ PartsReferenceImage.src }
+                            borderRadius={'1rem'}
+                        />
+                    </Link>
+                </Box>
+                <Box
+                    boxShadow={['0px 1px 5px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'} w={['30vw', '20vw']}>
+                    <Link href={'/minifigures'}>
+                        <Image
+                            src={ MinifiguresReferenceImage.src }
+                            borderRadius={'1rem'}
+                        />
+                    </Link>
+                </Box>
+            </Flex>
+            <Box my={['1vw']} mx={['1vw']} w={['98vw', '62vw']} maxW={'100vw'}
+                 boxShadow={['0px 1px 3px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'}>
+                <Link href={'/sets/series'}>
+                    <Image
+                        src={ SeriesReferenceImage.src }
+                        borderRadius={'1rem'}
+                    />
+                </Link>
+            </Box>
+        </Box>
+    );
+}
+
+export default IndexBanners;
--- store/pages/index.js
+++ store/pages/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Link from 'next/link';
-import { Box, Flex, Image } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { FooterBanner, Navbar, HeaderBanner, Footer } from "../components";
-import SetsReferenceImage from '../public/SetsReference.png';
-import PartsReferenceImage from '../public/PartsReference.png';
-import MinifiguresReferenceImage from '../public/MinifiguresReference.png';
-import SeriesReferenceImage from '../public/SeriesReference.png';
+import IndexBanners from "../components/IndexBanners";
 
 const Home = () => {
     return (
@@ -14,46 +10,7 @@ const Home = () => {
             <Box minH={'80vh'}>
                 <HeaderBanner reference={'/'}/>
 
-                <Box py={['10vw', '5vw', '0.5vw']}>
-                    <Flex mx={['3vw', '10.5vw']} w={['94vw', '62vw']} maxW={'100vw'} gap={['2vw', '1vw']}>
-                        <Box
-                            boxShadow={['0px 1px 5px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'} w={['30vw', '20vw']}>
-                            <Link href={'/sets/'}>
-                                <Image
-                                    src={ SetsReferenceImage.src }
-                                    borderRadius={'1rem'}
-                                />
-                            </Link>
-                        </Box>
-                        <Box
-                            boxShadow={['0px 1px 5px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'} w={['30vw', '20vw']}>
-                            <Link href={'/parts'}>
-                                <Image
-                                    src={ PartsReferenceImage.src }
-                                    borderRadius={'1rem'}
-                                />
-                            </Link>
-                        </Box>
-                        <Box
-                            boxShadow={['0px 1px 5px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'} w={['30vw', '20vw']}>
-                            <Link href={'/minifigures'}>
-                                <Image
-                                    src={ MinifiguresReferenceImage.src }
-                                    borderRadius={'1rem'}
-                                />
-                            </Link>
-                        </Box>
-                    </Flex>
-                    <Box my={['1vw']} mx={['1vw']} w={['98vw', '62vw']} maxW={'100vw'}
-                         boxShadow={['0px 1px 3px 1px rgba(0, 0, 0, 0.5)', '0px 3px 10px 2px rgba(0, 0, 0, 0.5)']} borderRadius={'1rem'}>
-                        <Link href={'/sets/series'}>
-                            <Image
-                                src={ SeriesReferenceImage.src }
-                                borderRadius={'1rem'}
-                            />
-                        </Link>
-                    </Box>
-                </Box>
+                <IndexBanners />
 
                 {/*<FooterBanner reference={'/'}/>*/}
             </Box>
